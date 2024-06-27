906b1da8726ad4020634fa478a7c6d388e8be590
Christhian <luizzchristhian@gmail.com>
2024-06-27 14:17:32 -0300

    code update, but it doesn't work

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('form-comparacao');
 
-const mensagemSucesso = `O 2º Número que é o ${n2.value} é maior que o 1º Número ${n1.value} e isso fez a ação ser um sucesso  ` 
+const n1 = document.getElementById('number-1').value;
+const n2 = document.getElementById('number-2').value;
+
+const mensagemSucesso = `O 2º Número que é o ${n2} é maior que o 1º Número ${n1} e isso fez a ação ser um sucesso  ` 
 const mensagemErro = 'Algo deu errado na transação, verifique se o 2º número é maior que o 1º número.'
 
 form.addEventListener('submit', function(e){
@@ -8,13 +11,10 @@ form.addEventListener('submit', function(e){
 })
 
 function validarCampo() {
-    const n1 = document.getElementById('number1');
-    const n2 = document.getElementById('number2');
+    const campoA = n1
+    const campoB = n2
 
-    const campoA = n1 (n1.value)
-    const campoB = n2 (n2.value)
-
-    if(campoB > campoA){
+    if (campoB > campoA){
         const containerMensagemSucesso = document.querySelector('#message-success');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
