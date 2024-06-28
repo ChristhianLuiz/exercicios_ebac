9d461ee4e12fbcaebbd3f94ca382bc9b55cfcbeb
Christhian <luizzchristhian@gmail.com>
2024-06-28 11:52:30 -0300

    update failure

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form-comparacao');
 
-const n1 = document.getElementById('number-1').value;
-const n2 = document.getElementById('number-2').value;
+const n1 = document.getElementById('number1').value;
+const n2 = document.getElementById('number2').value;
 
 const mensagemSucesso = `O 2º Número que é o ${n2} é maior que o 1º Número ${n1} e isso fez a ação ser um sucesso  ` 
 const mensagemErro = 'Algo deu errado na transação, verifique se o 2º número é maior que o 1º número.'
