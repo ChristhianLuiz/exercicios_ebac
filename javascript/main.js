8624f2e510d8cc3279aa233a58671ac27b73c003
Christhian <luizzchristhian@gmail.com>
2024-07-05 22:39:46 -0300

    update branch

--- javascript/main.js
+++ javascript/main.js
@@ -16,17 +16,17 @@ function validarCampo() {
     const campoB = n2
 
     if (campoB > campoA){
-        const containerMensagemSucesso = document.querySelector('#message-success');
+        const containerMensagemSucesso = document.querySelector('#success-message');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
     }
     else if(campoA > campoB){
-        const containerMensagemError = document.querySelector('#message-error');
+        const containerMensagemError = document.querySelector('#error-message');
         containerMensagemError.innerHTML = mensagemErro;
         containerMensagemError.style.display = 'block';
     }
     else{
-        const containerMensagemNeutra = document.querySelector('#message-error');
+        const containerMensagemNeutra = document.querySelector('#error-message');
         containerMensagemNeutra.innerHTML = mensagemErro;
         containerMensagemNeutra.style.display = 'block';
     }
